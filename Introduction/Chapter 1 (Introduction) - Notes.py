57d4310dcc329b85f077a11dab0d58a440b7feeb
Markus Grooss Karevold <markus.gk@outlook.com>
2023-09-21 17:43:22 +0200

    endringer

--- Introduction/Chapter 1 (Introduction) - Notes.py
+++ Introduction/Chapter 1 (Introduction) - Notes.py
@@ -1,1 +1,31 @@
 # Notes - Chapter 1: Introduction
+
+# Test imports and install nessecary libraries:
+from torchvision import transforms
+from torch.nn import functional as F
+from torch import nn
+from scipy.spatial import distance_matrix
+from PIL import Image
+import torchvision
+import torch
+import numpy as np
+import collections
+import hashlib
+import inspect
+import math
+import os
+import random
+import re
+import shutil
+import sys
+import tarfile
+import time
+import zipfile
+from collections import defaultdict
+import pandas as pd
+import requests
+from IPython import display
+from matplotlib import pyplot as plt
+from matplotlib_inline import backend_inline
+
+d2l = sys.modules[__name__]
